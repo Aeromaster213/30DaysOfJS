25d4b2c256cacbcc88abc536a3c5d43517766599
Arup Biswas <arup5abiswas@gmail.com>
2023-07-12 16:36:08 +0300

    day 1 done

--- dororo.js
+++ dororo.js
@@ -1,1 +1,2 @@
 console.log("Would you sacrifice anything, for your people?")
+console.log("Stand up with ypur strength, take it with your own hands.")
--- introduction.js
+++ introduction.js
@@ -1,8 +1,32 @@
 console.log('I am in your web browser')
 // primitive data types
 // String 'anything between these' "or these" `or these bacticks`
-// Number 
+// Number  12 , 3.141589, 0, 0.00, 
 // Boolean True False
 // undefined
 // Null
 // Symbol
+//console.log(firstName)
+let emptVal = null
+console.log(typeof tRuE)
+console.log(typeof 1)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof TRUE)
+let $change = 91 //changing value
+const nochange = 3 //notchanging
+var LotsOfLeak = 393.9 //bad, not reccomended
+//no nums as start, only $ _  special characters allowed
+let camelCaseOneHump = true
+let CamelCaseTwoHump = true //for classes
+// keyword varName assignmentOperator(=) assignedData
+let fN = 'Hyakkimaru'
+let lN = 'Daigo'
+let country = 'Japan'
+let city = null
+let age = 17
+let isMarried = false
+console.log(fN, lN, country, city, age, isMarried)
+const tan45Degree = 1
+const sunRisesFrom = 'East'
+console.log(tan45Degree, sunRisesFrom)
